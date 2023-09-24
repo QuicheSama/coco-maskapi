5c2580c04b1394ba7a368bfb3987993d91dc6f24
quichesama <kmbehari@gmail.com>
2023-09-24 02:38:52 -0700

    add: README and formatting:

--- src/lib/transpose.ts
+++ src/lib/transpose.ts
@@ -1,25 +1,25 @@
 // TODO: find a better way to do this (shape is vague, maybe just hard code `height` and `width?)
 const transpose = (data: Uint8ClampedArray, shape: Array<number>) => {
-    const expectedTotal = shape.reduce((acc, d) => acc * d, 1)
-    if(data.length !== expectedTotal) {
-        throw new Error(`data.length ${data.length} does not meet expected total (${expectedTotal})`)
-    }
+	const expectedTotal = shape.reduce((acc, d) => acc * d, 1);
+	if (data.length !== expectedTotal) {
+		throw new Error(`data.length ${data.length} does not meet expected total (${expectedTotal})`);
+	}
 
-    const transposedData = new Uint8ClampedArray(data.length);
+	const transposedData = new Uint8ClampedArray(data.length);
 
-    let destIndex = 0;
-    for (let x = 0; x < shape[1]; x++) {
-        for (let y = 0; y < shape[0]; y++) {
-            const srcIndex = (y * shape[1] + x) * 4;
-            transposedData[destIndex] = data[srcIndex];       // R
-            transposedData[destIndex + 1] = data[srcIndex + 1]; // G
-            transposedData[destIndex + 2] = data[srcIndex + 2]; // B
-            transposedData[destIndex + 3] = data[srcIndex + 3]; // A
-            destIndex += 4;
-        }
-    }
+	let destIndex = 0;
+	for (let x = 0; x < shape[1]; x++) {
+		for (let y = 0; y < shape[0]; y++) {
+			const srcIndex = (y * shape[1] + x) * 4;
+			transposedData[destIndex] = data[srcIndex]; // R
+			transposedData[destIndex + 1] = data[srcIndex + 1]; // G
+			transposedData[destIndex + 2] = data[srcIndex + 2]; // B
+			transposedData[destIndex + 3] = data[srcIndex + 3]; // A
+			destIndex += 4;
+		}
+	}
 
-    return transposedData;
-}
+	return transposedData;
+};
 
 export { transpose };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,29 @@
-
 <script lang="ts">
-    import { encode, decode } from '$lib';
-    import { rleFromString } from '$lib/mask';
+	import { encode, decode } from '$lib';
+	import { rleFromString } from '$lib/mask';
 	import { transpose } from '$lib/transpose';
-    let outputCanvas: HTMLCanvasElement;
-    let outputCanvasContext: CanvasRenderingContext2D | null;
+	let outputCanvas: HTMLCanvasElement;
+	let outputCanvasContext: CanvasRenderingContext2D | null;
 
-    // Sample taken from https://stackoverflow.com/questions/68838058/convert-coco-run-length-encoding-rle-back-to-segmentation
-    const sampleRleString = '_PX9330cc07O2N1j\\ODhb0=U]OEkb0;T]OFlb0:T]OFlb0:R]OHnb0d02N1O2N2N2M201O1O000006J2N001OEh\\OMYc03l\\OGUc08:0c\\OFTc09m\\OIPc08o\\OIPc09n\\OHQc0d00O1O0012N0O12\\Oi\\O031Vc0KV]O0[j:';
-    
-    const data = decode(sampleRleString, (filled: boolean) => filled ? [255, 255, 255, 255] : [0, 0, 0, 255])
-    
-    $: outputCanvasContext = outputCanvas?.getContext('2d')
-    $: outputCanvasContext && 
-        outputCanvasContext.putImageData(new ImageData(transpose(data, [529, 640, 4]), 529, 640), 0, 0);
+	// Sample taken from https://stackoverflow.com/questions/68838058/convert-coco-run-length-encoding-rle-back-to-segmentation
+	const sampleRleString =
+		'_PX9330cc07O2N1j\\ODhb0=U]OEkb0;T]OFlb0:T]OFlb0:R]OHnb0d02N1O2N2N2M201O1O000006J2N001OEh\\OMYc03l\\OGUc08:0c\\OFTc09m\\OIPc08o\\OIPc09n\\OHQc0d00O1O0012N0O12\\Oi\\O031Vc0KV]O0[j:';
+
+	const data = decode(sampleRleString, (filled: boolean) =>
+		filled ? [255, 255, 255, 255] : [0, 0, 0, 255]
+	);
+
+	$: outputCanvasContext = outputCanvas?.getContext('2d');
+	$: outputCanvasContext &&
+		outputCanvasContext.putImageData(new ImageData(transpose(data, [529, 640, 4]), 529, 640), 0, 0);
 </script>
-<div class='container'>
-    <canvas class='output' height={640} width={529} bind:this={outputCanvas}/>
+
+<div class="container">
+	<canvas class="output" height={640} width={529} bind:this={outputCanvas} />
 </div>
 
 <style>
-    .container {
-        display: flex;
-    }
+	.container {
+		display: flex;
+	}
 </style>
