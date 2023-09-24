07054ad897ba638c7ab1de38ac0d9d61a00fdff6
quichesama <kmbehari@gmail.com>
2023-09-23 23:52:23 -0700

    add: formatting:

--- src/lib/mask.ts
+++ src/lib/mask.ts
@@ -1,95 +1,97 @@
-import { stride } from "./stride";
+import { stride } from './stride';
 /**
- *  image's [height, width] 
- */ 
+ *  image's [height, width]
+ */
 type ImageDimensions = [number, number];
 
-const rleEncode = (imageData: Uint8ClampedArray, fillCheck: (pixelData: Uint8ClampedArray) => boolean, depth = 4) => {
-    const counts: Array<number> = [0];
-    stride(imageData, depth)
-        .map((pixelData) => fillCheck(pixelData))
-        .forEach((_, idx, mask) => {
-            if(idx === 0 && !mask[idx]) {
-                counts[counts.length - 1] += 1;
-                return;
-            }
-            if(mask[idx - 1] === mask[idx]) {
-                counts[counts.length - 1] += 1;
-            } else {
-                counts.push(1);
-            }
-        });
+const rleEncode = (
+	imageData: Uint8ClampedArray,
+	fillCheck: (pixelData: Uint8ClampedArray) => boolean,
+	depth = 4
+) => {
+	const counts: Array<number> = [0];
+	stride(imageData, depth)
+		.map((pixelData) => fillCheck(pixelData))
+		.forEach((_, idx, mask) => {
+			if (idx === 0 && !mask[idx]) {
+				counts[counts.length - 1] += 1;
+				return;
+			}
+			if (mask[idx - 1] === mask[idx]) {
+				counts[counts.length - 1] += 1;
+			} else {
+				counts.push(1);
+			}
+		});
 
-    return counts;
+	return counts;
 };
 
 const rleDecode = (rle: Array<number>, makePixel: (filled: boolean) => Array<number>) => {
-    const result: Array<number> = []
-    let filled = false;
-    
-    for(const count of rle) {
-        console.log('count', count);
-        for(let idx = 0; idx < count; idx++) {
-            const pixel = makePixel(filled)
-            console.log(`${idx}, pixel value`, pixel);
-            result.push(...pixel)
-        }
-        filled = !filled;
-    }
-    return new Uint8ClampedArray(result);
-}
+	const result: Array<number> = [];
+	let filled = false;
+
+	for (const count of rle) {
+		console.log('count', count);
+		for (let idx = 0; idx < count; idx++) {
+			const pixel = makePixel(filled);
+			console.log(`${idx}, pixel value`, pixel);
+			result.push(...pixel);
+		}
+		filled = !filled;
+	}
+	return new Uint8ClampedArray(result);
+};
 
 const rleToString = (rle: Array<number>) => {
-    let result = '';
-    for(let idx = 0; idx < rle.length; idx++) {
-        let count = rle[idx]
-        let more = true;
+	let result = '';
+	for (let idx = 0; idx < rle.length; idx++) {
+		let count = rle[idx];
+		let more = true;
 
-        while(more) {
-            let charCode = count & 0x1f;
-            count >>= 5;
+		while (more) {
+			let charCode = count & 0x1f;
+			count >>= 5;
 
-            more = (charCode & 0x10) ? count !== -1 : count !== 0;
-            
-            if(more) charCode |= 0x20;
-            result += String.fromCharCode(charCode + 48);
-        }
-    }
+			more = charCode & 0x10 ? count !== -1 : count !== 0;
 
-    return result;
-}
+			if (more) charCode |= 0x20;
+			result += String.fromCharCode(charCode + 48);
+		}
+	}
+
+	return result;
+};
 
 const rleFromString = (encodedString: string) => {
-    const characterCodes = encodedString.split('').map(char => char.charCodeAt(0));
+	const characterCodes = encodedString.split('').map((char) => char.charCodeAt(0));
 
-    let codePointer = 0;
-    let ith = 0;
-    let count = 0;
-    let more = 0;
-    
-    const counts: Array<number> = [];
+	let codePointer = 0;
+	let ith = 0;
+	let count = 0;
+	let more = 0;
 
-    while(characterCodes[codePointer]) {
-        count = 0;
-        ith = 0;
-        more = 1;
+	const counts: Array<number> = [];
 
-        while(more) {
-            const char = characterCodes[codePointer] - 48
-            count |= (char & 0x1f) << 5 * ith;
-            more = char & 0x20;
-            codePointer++;
-            ith++;
-            if(!more && (char & 0x10))
-                count |= -1 << 5 * ith;
-        }
+	while (characterCodes[codePointer]) {
+		count = 0;
+		ith = 0;
+		more = 1;
 
-        counts.push(count);
-    }
+		while (more) {
+			const char = characterCodes[codePointer] - 48;
+			count |= (char & 0x1f) << (5 * ith);
+			more = char & 0x20;
+			codePointer++;
+			ith++;
+			if (!more && char & 0x10) count |= -1 << (5 * ith);
+		}
 
-    return counts;
-}
+		counts.push(count);
+	}
 
+	return counts;
+};
 
 export { rleEncode, rleDecode, rleToString, rleFromString };
 export type { ImageDimensions };
