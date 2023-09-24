538fb131830247b39ab15d7f6f5de8cc3766c8d1
quichesama <kmbehari@gmail.com>
2023-09-24 00:01:17 -0700

    add: formatting

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,15 +1,19 @@
 // place files you want to import through the `$lib` alias in this folder.
 
-import { rleEncode, rleDecode, rleFromString, rleToString } from "./mask"
+import { rleEncode, rleDecode, rleFromString, rleToString } from './mask';
 
-const encode = (imageData: Uint8ClampedArray, fillCheck: (pixelData: Uint8ClampedArray) => boolean, depth = 4) => {
-    const rle = rleEncode(imageData, fillCheck, depth);
-    return rleToString(rle);
-}
+const encode = (
+	imageData: Uint8ClampedArray,
+	fillCheck: (pixelData: Uint8ClampedArray) => boolean,
+	depth = 4
+) => {
+	const rle = rleEncode(imageData, fillCheck, depth);
+	return rleToString(rle);
+};
 
 const decode = (encodedString: string, makePixel: (filled: boolean) => Array<number>) => {
-    const rle = rleFromString(encodedString);
-    return rleDecode(rle, makePixel);
-}
+	const rle = rleFromString(encodedString);
+	return rleDecode(rle, makePixel);
+};
 
 export { encode, decode };
